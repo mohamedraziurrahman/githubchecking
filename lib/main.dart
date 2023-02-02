bc0c960b8ca9e658b65a7c13bc4eed720dcdbf2c
Mohamed Raziur Rahman M <mohamedraziurrahman@gmail.com>
2023-02-02 21:51:06 +0530

    google sign in code

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,42 @@
+// main.dart file
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'SignInScreen.dart';
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+
+// initializing the firebase app
+await Firebase.initializeApp();
+
+// calling of runApp
+runApp(GoogleSignIn());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+class GoogleSignIn extends StatefulWidget {
+GoogleSignIn({Key? key}) : super(key: key);
+@override
+_GoogleSignInState createState() => _GoogleSignInState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class _GoogleSignInState extends State<GoogleSignIn> {
+@override
+Widget build(BuildContext context) {
+	
+	// we return the MaterialApp here ,
+	// MaterialApp contain some basic ui for android ,
+	return MaterialApp(
+	
+	//materialApp title
+	title: 'GEEKS FOR GEEKS',
+	debugShowCheckedModeBanner: false,
+	theme: ThemeData(
+		primarySwatch: Colors.green,
+	),
+	
+	// home property contain SignInScreen widget
+	home: SignInScreen(),
+	);
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this chSDFJDSHKJTESIS',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'DECREMENT',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
 }
